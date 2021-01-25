230f505e5bea00fa06439323f95efa55ed4421ac
Akram <aminakram379@gmail.com>
2021-01-26 01:08:03 +0200

    .

---
+++ heighly-responsive/js/js.js
@@ -0,0 +1,22 @@
+/// <reference path="./jquery.js" />
+
+
+
+
+
+$(()=>{
+
+
+    $("div.circ").click((e)=>{
+
+        console.log(
+            $(e.target)
+        )
+        
+    })
+
+
+
+})
+
+
